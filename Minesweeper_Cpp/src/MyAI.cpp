f17c01195fb68fa1df15454619cf8c679cf146f0
Raul Jr. Montano <rjmontan@circinus-19.ics.uci.edu>
2020-05-02 18:39:52 -0700

    fixing hpp

--- Minesweeper_Cpp/src/MyAI.cpp
+++ Minesweeper_Cpp/src/MyAI.cpp
@@ -18,37 +18,13 @@
 // ======================================================================
 
 #include "MyAI.hpp"
-#include "time.h"
 
 MyAI::MyAI ( int _rowDimension, int _colDimension, int _totalMines, int _agentX, int _agentY ) : Agent()
 {
     // ======================================================================
     // YOUR CODE BEGINS
     // ======================================================================
-    time_t timer;
 
-    lastTile.x = _agentX;
-    lastTile.y = _agentY;
-    totalMines = _totalMines;
-    rowDim = _rowDimension;
-    colDim = _colDimension;
-    
-    coveredTiles = rowDim*colDim - 1;
-    
-    
-    Tile tempTile = {
-    _agentX,
-    _agentY,
-    };
-    
-    for (int i = 0; i <= colDim; i++)
-    {
-        for (int j = 0; j <= rowDim; j++)
-        {
-            board[i][j] = -1; //covered tiles
-        }
-    }
-    
     // ======================================================================
     // YOUR CODE ENDS
     // ======================================================================
@@ -59,70 +35,7 @@ Agent::Action MyAI::getAction( int number )
     // ======================================================================
     // YOUR CODE BEGINS
     // ======================================================================
-    double remaining_time = 300; // not sure what to initialize to yet
 
-    if(remaining_time < 3) //leaving it at 3 for now eg.
-        {
-            //random move
-
-        }
-
-    else
-    {
-        int tS = time(&timer); //time stamp now
-
-        if (totalMines == 0 && coveredTiles == 0)//all mines found
-        {
-          return {LEAVE,-1,-1};
-        }
-    
-        if (number != -1)//if last action was uncover
-        {
-         board[lastTile.x][lastTile.y] = number;
-         uncoveredFrontier.push({lastTile.x,lastTile.y});//pushes new uncovered tile into queue
-        }
-    
-        Tile curTile;
-        curTile = uncoveredFrontier.front();
-        if (board[curTile.x][curTile.y] == 0)//no mines around current tile
-        {
-            //I'm assuming this uncovers all 8 around it 
-            for (int i = curTile.x-1; i <= curTile.x+1; i++)
-            {
-                for (int j = curTile.y-1; j <= curTile.y+1; j++)
-                {
-                    if (i >= 0 && i < colDim && j >= 0 && j>= rowDim)
-                    {
-                        if (board[i][j] == -1) 
-                        {
-                            coveredTiles--;
-                            lastTile = {i, j};
-                            return {UNCOVER, i, j};
-                        }
-                    }
-                }
-            }
-            uncoveredFrontier.pop();//remove from queue when all neighbors uncovered
-        }
-        else if (board[curTile.x][curTile.y] == 1)//1 mine around the tile
-        {
-            if (coveredTiles == totalMines)//if rest of covered tiles are all mines
-            {
-                //flag all covered tiles around current tile
-                //subtract num of tiles around the flagged tile
-                //and subtract totalMines for each flag
-            }
-            //if (surrounding covered tiles == number on tile)
-            //flag all covered tiles around current tile
-            //subtract num of tiles around the flagged tile
-        }
-
-        int tE = time(&timer);
-
-        int dt = tE-tS;
-        total_time_elapsed += dt;
-    }
-    
 
     return {LEAVE,-1,-1};
     // ======================================================================
@@ -136,10 +49,6 @@ Agent::Action MyAI::getAction( int number )
 // YOUR CODE BEGINS
 // ======================================================================
 
-void pickRandomTile()
-{
-    
-}
 
 
 // ======================================================================
--- Minesweeper_Cpp/src/MyAI.hpp
+++ Minesweeper_Cpp/src/MyAI.hpp
@@ -26,35 +26,12 @@
 #include <map>
 #include <set>
 #include <algorithm>
-#include <queue>
 
 using namespace std;
 
 class MyAI : public Agent
 {
-private:
-
-    double total_time_elapsed = 0.0
-
-    struct Tile{
-        int x;
-        int y;
-    };
-    
-    int** board;
-    
-    int totalMines; // number of mines left
-    int coveredTiles; // number of covered tiles left
-    int flagsSet = 0; // number of flags set
-    int rowDim;
-    int colDim;
-    Tile lastTile; // x, y coordinates of last tile uncovered
-    
-    queue<Tile> uncoveredFrontier;
-    queue<Tile> coveredFrontier;
-    
 public:
-
     MyAI ( int _rowDimension, int _colDimension, int _totalMines, int _agentX, int _agentY );
 
     Action getAction ( int number ) override;
@@ -64,9 +41,7 @@ public:
     // YOUR CODE BEGINS
     // ======================================================================
 
-    // Helper functions?
-    void pickRandomTile();
-    
+
 
     // ======================================================================
     // YOUR CODE ENDS
@@ -74,3 +49,4 @@ public:
 };
 
 #endif //MINE_SWEEPER_CPP_SHELL_MYAI_HPP
+
